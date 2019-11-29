46dcc6fad75b6bb3e3be0396c34c63903e6595a9
Lucas Fénart <lucas.fenart@gmail.com>
2019-11-29 13:00:18 +0100

    ref #3633 add actions and result in Test

--- src/main/java/fr/uha/ensisa/gl/cmwfb/mantest/Test.java
+++ src/main/java/fr/uha/ensisa/gl/cmwfb/mantest/Test.java
@@ -1,9 +1,19 @@
 package fr.uha.ensisa.gl.cmwfb.mantest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 
 	private long id;
 	private String name;
+	private List<String> actions;
+	private String result;
+	
+	public Test() {
+		this.actions = new ArrayList<String>();
+		this.result = "";
+	}
 
 	public long getId() {
 		return this.id;
@@ -21,5 +31,28 @@ public class Test {
 		this.name = name;
 	}
 	
+	public List<String> getActions() {
+		return this.actions;
+	}
+	
+	public void addAction(String action) {
+		this.actions.add(action);
+	}
+	
+	public void addAction(int index, String action) {
+		this.actions.add(index, action);
+	}
+	
+	public void removeAction(int index) {
+		this.actions.remove(index);
+	}
+	
+	public String getResult() {
+		return this.result;
+	}
+	
+	public void setResult(String result) {
+		this.result = result;
+	}
 
 }
