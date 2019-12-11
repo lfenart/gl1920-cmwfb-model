f7ad03406792b89e0c34056d09b92a2635c66be4
Lucas Fénart <lucas.fenart@gmail.com>
2019-12-11 16:02:23 +0100

    ref #3633 Add class Step

---
+++ src/main/java/fr/uha/ensisa/gl/cmwfb/mantest/Step.java
@@ -0,0 +1,17 @@
+package fr.uha.ensisa.gl.cmwfb.mantest;
+
+public class Step {
+	private String text;
+	
+	public Step() {
+		this.text = "";
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public String getText() {
+		return this.text;
+	}
+}
